83939cadcb97f67a0e12d26b15c992a7e4887521
Kevin Kraft <kkraft7@gmail.com>
2020-07-05 21:41:04 -0700

    1. Updated the Note class to extend NoteElement instead of Node. 2. Added code to track and retrieve Note elements 3. Added assertions and new tests

--- src/main/java/server/Note.java
+++ src/main/java/server/Note.java
@@ -3,7 +3,7 @@ package server;
 import server.element.NoteElement;
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.*;
 
 // Need tags to indicate relevant search categories
 /**
@@ -11,15 +11,16 @@ import java.util.ArrayList;
  */
 @Entity
 @Table(name = "NOTES")
-public class Note extends Node<Note> {
+public class Note extends NoteElement {
     @Id
     @Column(name="NOTE_ID")
     @GeneratedValue
     private Long noteId;
     private String description;
     private final LocalDateTime created;
-    // ToDo: Change this to a map?
+    // Using "? extends NoteElement" here produces errors...
     private ArrayList<NoteElement> elements;
+    private Map<Class<? extends NoteElement>, List<Integer>> elementLocator;
 
     // ToDo: Will this update the created date every time a note is retrieved from the DB?
     protected Note() {
@@ -32,6 +33,8 @@ public class Note extends Node<Note> {
         this.description = desc;
         created = LocalDateTime.now();
         elements = new ArrayList<>();
+        // This contains a map from Note element class to element indexes in the elements list
+        elementLocator = new HashMap<Class<? extends NoteElement>, List<Integer>>();
     }
 
     public Note(String title) { this(title, null); }
@@ -40,15 +43,65 @@ public class Note extends Node<Note> {
     public void setDescription(String newDescription) { description = newDescription; }
     public LocalDateTime getDateCreated() { return created; }
 
-    public void addNote(Note newNote) { addChildItem(newNote); }
-    // Add deleteNote()?
-
+    // ToDo: Can I delete this?
     public ArrayList<NoteElement> getElements() { return elements; }
-    public void addElement(NoteElement newElement) { elements.add(newElement); }
+
+    /**
+     * Add new Note element at the end of the current note.
+     * @param newElement The new Note element to be added
+     */
+    public void addElement(NoteElement newElement) {
+        // elements.add(newElement);
+        addElement(newElement, elements.size());
+    }
+
+    /**
+     * Add new Note element at the specified location.
+     * @param newElement The new Note element to be added
+     * @param index The location (0-based) of the new element
+     */
+    public <T extends NoteElement> void addElement(T newElement, int index) {
+        elements.add(index, newElement);
+        updateElementLocator(newElement.getClass(), index);
+    }
+
+    /**
+     * Return the requested element from the list of {@link Note} elements.
+     * The index refers to the position of the {@link Note} element in a list of elements
+     * of the same type. So, for example, if the {@link Note} contains two Contacts index
+     * 0 would refer to the first one and index 1 would refer to the second (regardless
+     * of their position in the overall list of {@link Note} elements).
+     *
+     * ToDo: Describe how this works...
+     * @param elementClass The class of the {@link Note} element to be retrieved
+     * @param index The 0-based index of the {@link Note} element to be retrieved
+     *              (for elements of the same class type)
+     * @return The requested {@link Note} element (or an exception)
+     */
+    public NoteElement getElement(Class<? extends NoteElement> elementClass, int index) {
+        if (!elementLocator.containsKey(elementClass)) {
+            throw new RuntimeException("No " + elementClass.getSimpleName() + " element found in this note");
+        }
+        if (index >= elements.size()) {
+            throw new RuntimeException("Invalid index (" + index + ") for note element list (size "
+                    + elements.size() + ")");
+        }
+        // ToDo: Do a sanity check that the returned element is the right class?
+        return elements.get(elementLocator.get(elementClass).get(index));
+    }
+
+    private void updateElementLocator(Class<? extends NoteElement> elementClass, int index) {
+        if (!elementLocator.containsKey(elementClass)) {
+            elementLocator.put(elementClass, new ArrayList<>());
+        }
+        elementLocator.get(elementClass).add(index);
+    }
+
+    // Add deleteElement()?
 
     @Override
     public String toString() {
-        String result = "Note:\n" + name;
+        String result = "Note:\n" + getName();
         if (description != null && !description.equals("")) {
             result += "\n" + description;
         }
@@ -56,12 +109,16 @@ public class Note extends Node<Note> {
             // ToDo: Warn if element is null?
             result = result.concat("\n\n" + elem);
         }
-        if (getChildItems().size() > 0) {
-            result += "\n\nSub Notes:\n";
-            for (Node<Note> item : getChildItems()) {
-                result = result.concat("\n" + item + "\n");
-            }
-        }
         return result;
     }
+
+    // For testing
+    protected NoteElement getElement(int index) { return elements.get(index); }
+
+    protected void clear() {
+        elements.clear();
+        elementLocator.clear();
+    }
+
+    // Add clearNote() to get back to original "before" state
 }
--- src/test/java/server/NoteTest.java
+++ src/test/java/server/NoteTest.java
@@ -3,38 +3,83 @@ package server;
 import server.element.*;
 import java.net.MalformedURLException;
 import org.junit.jupiter.api.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NoteTest {
+    private static Note testNote;
     private static Note subNote1;
     private static Note subNote2;
 
     @BeforeAll
-    public static void setUpTest() throws MalformedURLException {
+    public static void setUpTests() throws MalformedURLException {
+        testNote = new Note("Test Note", "Description for Test Note");
         subNote1 = new Note("Test sub-note 1", "Description for sub-note 1");
         subNote1.addElement(new MediaItem("I'm Your Captain", "Grand Funk", MediaItem.Category.SONG));
         subNote2 = new Note("Test sub-note 2", "Description for sub-note 2");
         subNote2.addElement(new Link("Google", "http://facebook.com"));
     }
 
+    @AfterEach
+    public void testCleanup() {
+        testNote.clear();
+    }
+
     @Test
     public void createNoteWithEachElementType() throws MalformedURLException {
-        Note testNote = new Note("Test Note 1", "Description for Test Note 1");
-
         testNote.addElement(new Contact("Contact1", "234-5678", "123 4th St.", "Menlo Park"));
         testNote.addElement(new MediaItem("Blind Alley", "Fanny", MediaItem.Category.SONG));
         testNote.addElement(new Price(5.00));
         testNote.addElement(new Link("Google", "http://google.com"));
         testNote.addElement(new EventInfo("Deadline", null, "2020-06-12 00:00:00"));
-        System.out.println(testNote);
+
+        // System.out.println(testNote);
+        // ToDo: Use fluent assertions (no need for Hamcrest matchers)?
+        // assertThat("Expected Contact type at position 0", testNote.getElement(0));
+        assertSame("Expected Contact type at position 0",
+                testNote.getElement(0), testNote.getElement(Contact.class, 0));
+        assertSame("Expected Contact type at position 0",
+                testNote.getElement(1), testNote.getElement(MediaItem.class, 0));
+        assertSame("Expected Contact type at position 0",
+                testNote.getElement(2), testNote.getElement(Price.class, 0));
+        assertSame("Expected Contact type at position 0",
+                testNote.getElement(3), testNote.getElement(Link.class, 0));
+        assertSame("Expected Contact type at position 0",
+                testNote.getElement(4), testNote.getElement(EventInfo.class, 0));
     }
 
     @Test
-    public void createNoteWithSubNotes() {
-        Note testNote = new Note("Test Note 2", "Description for Test Note 2");
-        testNote.addNote(subNote1);
-        testNote.addNote(subNote2);
-        System.out.println(testNote);
+    public void createNoteWithTwoSubNotes() {
+        testNote.addElement(subNote1);
+        testNote.addElement(subNote2);
+
+        // System.out.println(testNote);
+        assertSame("Expected Contact type at position 0",
+                testNote.getElement(0), testNote.getElement(Note.class, 0));
+        assertSame("Expected Contact type at position 0",
+                testNote.getElement(1), testNote.getElement(Note.class, 1));
     }
 
+    @Test
+    public void tryGettingInvalidClassTypeFromElementList() {
+        Exception ex = assertThrows(RuntimeException.class, () -> testNote.getElement(Note.class, 0));
+        // System.out.println("Exception message: " + ex.getMessage());
+        String expectedMessage = "No Note element found";
+        assertTrue("Exception message contains " + expectedMessage,
+                ex.getMessage().contains(expectedMessage));
+    }
+
+    @Test
+    public void tryGettingInvalidElementIndexFromElementList() throws MalformedURLException {
+        testNote.addElement(new Link("Google", "http://facebook.com"));
+
+        Exception ex = assertThrows(RuntimeException.class, () -> testNote.getElement(Link.class, 1));
+        // System.out.println("Exception message: " + ex.getMessage());
+        String expectedMessage = "Invalid index (1) for note element list (size 1)";
+        assertEquals("Exception message equals " + expectedMessage, ex.getMessage(), expectedMessage);
+    }
+
+    // ToDo: Add elements at a specified location
+    // ToDo: More negative tests?
     // ToDo: Test adding duplicate notes?
 }
